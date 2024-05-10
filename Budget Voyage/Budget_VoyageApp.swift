b3b9140a36bbc4358bb8c67f9568ce4936d9c0c3
Olga <luna33040@gmail.com>
2024-05-10 15:05:33 -0400

    Initial Commit

---
+++ Budget Voyage/Budget_VoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_VoyageApp.swift
+//  Budget Voyage
+//
+//  Created by Olga on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Budget_VoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
